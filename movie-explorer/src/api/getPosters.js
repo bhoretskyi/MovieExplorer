a541a4b30dad67302b4fbf4dab8edf8771a9ae19
BHO <bhoretskyi@gmail.com>
2025-05-23 14:57:06 +0200

    add https

--- movie-explorer/src/api/getPosters.js
+++ movie-explorer/src/api/getPosters.js
@@ -2,6 +2,6 @@ import axios from "axios";
 const API_KEY = "912f8b69";
 
 export const getPosters = async (title) => {
-  const res = await axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${title}`);
+  const res = await axios.get(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${title}`);
   return res.data;
 };
