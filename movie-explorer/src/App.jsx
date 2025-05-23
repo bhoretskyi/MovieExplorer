afc4ac4ff5fba40f4a84956d11f905d0725f44e0
BHO <bhoretskyi@gmail.com>
2025-05-23 14:52:35 +0200

    add test movies fetch

--- movie-explorer/src/App.jsx
+++ movie-explorer/src/App.jsx
@@ -1,35 +1,26 @@
-// import { useState } from 'react'
-import './App.css'
-import { getPosters } from './api/getPosters'
-
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getPosters } from "./api/getPosters";
 function App() {
-  getPosters()
-
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    getPosters("cat").then((data) => {
+      console.log(data.Search);
+      setMovie(data.Search);
+    });
+  }, []);
   return (
     <>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
       <h1>Media Explorer</h1>
+      {movie.map((movie) => (
+        <div key={movie.imdbID}>
+          <h2>{movie.Title}</h2>
+          <p>{movie.Year}</p>
+          <img src={movie.Poster} alt={movie.Title} />
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- movie-explorer/src/api/getPosters.js
+++ movie-explorer/src/api/getPosters.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 const API_KEY = "912f8b69";
 
-export const getPosters = async (title = 'women') => {
+export const getPosters = async (title) => {
   const res = await axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${title}`);
-  console.log(res)
   return res.data;
 };
