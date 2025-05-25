56b538317ba70ff4fd986581ed8288d82aa65f7b
BHO <bhoretskyi@gmail.com>
2025-05-25 15:27:17 +0200

    add theme provider

--- movie-explorer/src/App.jsx
+++ movie-explorer/src/App.jsx
@@ -2,7 +2,9 @@ import "./App.css";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import { MovieList } from "./components/MovieList.jsx";
 import { Favorites } from "./components/Favorites.jsx";
+import { useTheme } from "./hooks/ThemeContext.jsx";
 function App() {
+  const { theme, toggleTheme } = useTheme();
   return (
     <BrowserRouter basename="/MovieExplorer">
       <>
@@ -10,6 +12,9 @@ function App() {
           <Link to="/">Movie Explorer</Link>
           <Link to="/favorites">Favorite Movies</Link>
         </nav>
+        <button type="button" onClick={toggleTheme}>
+          theme({theme === "dark" ? "light" : "dark"})
+        </button>
         <Routes>
           <Route path="/" element={<MovieList />} />
           <Route path="/favorites" element={<Favorites />} />
